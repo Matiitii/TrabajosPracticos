0aea4b90a1e165d77059a9394f187764a6371f1b
Matii <166016459+Matiitii@users.noreply.github.com>
2026-03-26 11:12:43 -0300

    Add files via upload

---
+++ tp2/js.js
@@ -0,0 +1,17 @@
+//Act 1
+
+let frutas = ['Frutilla', 'Banana', 'Granada']
+
+frutas.push('Manzana')
+frutas.unshift('Naranja')
+
+frutas.pop()
+frutas.shift()
+
+console.log(frutas)
+
+//Act2
+
+let nombres = ['Luisana', 'Hannah', 'Maylén']
+
+console.log(frutas.length)
